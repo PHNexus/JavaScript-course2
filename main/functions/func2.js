97b5585d4b4cf4563a5e834b2bbb1e9ef6b4c7a5
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-02-12 09:40:31 -0300

    Operadores

---
+++ main/functions/func2.js
@@ -0,0 +1,17 @@
+//teste de funcoes
+
+
+let Cor = 'Azul'
+
+
+function trocarCor(){
+
+Cor = 'Vermelho'
+
+}
+
+console.log(Cor)
+trocarCor()
+console.log(Cor)
+
+
---
+++ main/functions/operadores.js
@@ -0,0 +1,30 @@
+// Operadores Aritimeticos(matematicos)
+// Operadores Atribuicao
+// Operadores de comparacao
+// Operadores Logicos
+// Operadores Bitwise
+
+let salario = 100;
+
+console.log(salario + salario) // adicao
+console.log(salario - salario) // subtracao
+console.log(salario * salario) // multiplacao
+console.log(salario / salario) // divisao
+console.log(5 ** 5) // expontencial // potencia
+
+// ++ --  operadores de incliento e decremeto
+
+let idade = 16
+console.log(++idade) // incrementar +1
+console.log(--idade) // descrementar / diminuir -1
+// 16 + 1 = 17   17-1 =16
+
+//sempre use === sera comparado para ser identico
+// operadores de igualdade
+// Igualdade estria
+
+console.log(1 === 1) // esta comparando e o tipo tem q ser identico
+console.log('1' === 1) // tipo diferente
+
+// igualdade solta
+console.log('1' == 1) // vai comparar apenas o numero = true
