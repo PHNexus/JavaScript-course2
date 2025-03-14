ad05f42d53e7e793481b2f837fc400b424222c0c
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-14 11:08:18 -0300

    array, funcoes juntos
    nao deu mt certo mas vou aprendendo

--- main/functions/func3.js
+++ main/functions/func3.js
@@ -1,10 +1,14 @@
-let habilidades = [ fogo , antimateria , ar]
-function habilidaderecebida (){
+let habilidades = ['fogo','antmateria','ar']
+function habilidaderecebida (recebeu){
 
-console.log('voce recebeu'[1])
+recebeu = 'voce recebeu'
 
 }
 
 console.log(habilidades)
-habilidaderecebida
-console.log(habilidades)
+habilidaderecebida()
+console.log(habilidades[1])
+
+// meio confuso nunca meti funcoes com array
+// vou voltar pra estudar mais um pouco e colocar
+//  a maneira certa abaixo
