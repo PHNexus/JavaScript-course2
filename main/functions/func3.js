31ae33ab32fb226c7203fd29c85f91517391e58d
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-14 10:58:10 -0300

    funcoes

---
+++ main/functions/func3.js
@@ -0,0 +1,10 @@
+let habilidades = [ fogo , antimateria , ar]
+function habilidaderecebida (){
+
+console.log('voce recebeu'[1])
+
+}
+
+console.log(habilidades)
+habilidaderecebida
+console.log(habilidades)
