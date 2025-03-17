7494b27a22100c155a95da564418ea2ea6df1fc3
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-17 08:31:10 -0300

    alterei o nome e criei uma pasta

--- main/functions/tipsfunc.js
+++ main/functions/tipsfunc.js
@@ -10,23 +10,24 @@ function multiplicarPorDois(valor) {
   return valor * 2;
 }
 
-console.log(multiplicarPorDois(5));
+console.log(multiplicarPorDois(5)); // 2 x 5 = 10
 
 // faz um calculo operacao e retorna algo
 function calculadora(valor) {
   return valor * 3;
 }
 
-console.log(calculadora(10));
-
+console.log(calculadora(10)); // 10 x 3 = 30
+calculadora()
 //praticando calculadora
 
 function calculadora3(calculadora3) {
   return calculadora3 * 2;
 }
-
-console.log(calculadora3(15));
+calculadora3()
+console.log(calculadora3(15)); // 15 x 2 = 30
 
 function calculate(somar) {
   return somar * 5;
 }
+console.log(calculate(5)) //5 x 5 = 25
