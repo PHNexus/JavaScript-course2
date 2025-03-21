651ffa5af801ca5e49b012887888d681fc41f498
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-21 11:56:22 -0300

    exercicios

---
+++ main/functions/exercfunc/exercfunc4.js
@@ -0,0 +1,21 @@
+function multiplicacao(valor) {
+  return valor * 2;
+}
+console.log(multiplicacao(10));
+
+function dividir(valor){
+    return valor / 2
+}
+console.log(dividir(10)) // ira retornar 5
+
+
+function potencia(valor){
+    return valor ** 2
+}
+console.log(potencia(5)) // == 5 x 5 = 25
+
+function subtrair(valor){
+    return valor - 2
+}
+
+console.log(subtrair(10)) // == 10 - 2 = 8
