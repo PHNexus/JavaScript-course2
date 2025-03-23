1714f5b628cff3503c05d3580e66d583acb31520
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-23 12:21:54 -0300

    o

---
+++ main/operadores/operadores3.js
@@ -0,0 +1,9 @@
+// operador logico e (&&)
+// retorna true se os dois operadores forem true
+// retorna false em diante se forem diferentes
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
