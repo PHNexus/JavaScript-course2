a623e2cff31eceab43c4ed0b6e9c831ee93dc835
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-26 11:54:43 -0300

    funcoes + if else

---
+++ main/if-else/exercIF/script.js
@@ -0,0 +1,22 @@
+let numero = 9
+function somar(valor){
+    return numero + valor
+}
+let resultado = somar(5)
+
+if(resultado > 10){
+    console.log('sim o numero e maior que 10')
+    }else{
+        console.log('não o numero e menor ou igual a 10')
+    }
+
+    
+   /* Como o codico funciona
+  1 Define a variável numero como 9.
+
+  2 Cria a função somar(valor), que retorna a soma de numero e valor.
+    
+  3 Chama somar(5), resultando em 9 + 5 = 14.
+    
+  4 Como resultado (14) é maior que 10, imprime "sim o numero e maior que 10" no console. 
+  */
