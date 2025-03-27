6da67bb395d2d9041b2e8a815c643d0e3e447aa6
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-27 19:50:11 -0300

    Logica

--- main/functions/exercfunc/exercfunc2.js
+++ main/functions/exercfunc/exercfunc2.js
@@ -1,7 +1,16 @@
-// funcao que nao retorna nada
-function dizerNome() {
-  console.log("Marcos");
-}
-dizerNome();
-
+console.log("Marcos")
 // usando if
+let placa = "1B25";
+let placaOrig = placa;
+function alterarPlaca() {
+  placa = "1E25";
+}
+alterarPlaca();
+
+if (placa === placaOrig) {
+  console.log("Placa Esta Correta");
+} else {
+  console.log("Placa Esta Incorreta");
+}
+// ira comparar se o valor da placa e identico a placaOrig
+// mas ira retornar incorreto por  que a funcao altera o valor
