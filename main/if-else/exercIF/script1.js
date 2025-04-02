b88420863bf6256dcfd44cd1fd0c041ecdb10da3
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-04-02 17:41:14 -0300

    indent

--- main/if-else/exercIF/script1.js
+++ main/if-else/exercIF/script1.js
@@ -1,10 +1,10 @@
-let idade = 11
-function somar(valor){
-    return idade + valor;
-} 
-let resultado = somar(5)
-if (resultado >= 18){
-    console.log("É maior de idade");
-}else{
-    console.log("É menor de idade");
+let idade = 11;
+function somar(valor) {
+  return idade + valor;
+}
+let resultado = somar(5);
+if (resultado >= 18) {
+  console.log("É maior de idade");
+} else {
+  console.log("É menor de idade");
 }
