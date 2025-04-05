cfbda27a8fb573b185946add7d68b665978da3a4
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-04-05 04:00:16 -0300

    tipos de dados

---
+++ main/operadores/tiposDados.js
@@ -0,0 +1,8 @@
+// tipos de dados
+var inteiro = (1,3,-7,198,0) // integer
+var real = (15.9,-77.3)  // float
+var string = ("Tipo,Texto") // string
+var logico = ('true','false') // boolean
+var vetor =  ["carlos", falso, "Cachorro", 65,5] // array
+/*     0        1        2        3    */ //indice
+
